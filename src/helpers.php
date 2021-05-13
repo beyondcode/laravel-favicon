ca411ce81a66408b38bf491198156945d77f4c82
Lloric Mayuga Garcia <lloricode@gmail.com>
2021-05-14 07:54:11 +0800

    Fix trim

--- src/helpers.php
+++ src/helpers.php
@@ -6,7 +6,7 @@ if (! function_exists('favicon')) {
     function favicon($image)
     {
         if (app(FaviconGenerator::class)->shouldGenerateFavicon(app()->environment())) {
-            return rtrim(config('app.url'), '/').'/'.config('favicon.url_prefix')."/$image";
+            return rtrim(config('app.url'), '/').'/'.config('favicon.url_prefix').'/'.trim($image,'/');
         }
 
         return $image;
